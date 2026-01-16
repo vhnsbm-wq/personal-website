dabbda14479e1ee8a4672da316f449305af71c4e
vhnsbm-wq <vhnsbm@gmail.com>
2026-01-16 19:11:40 +0800

    Add files via upload

---
+++ kf/personal website/src/data/skills.ts
@@ -0,0 +1,20 @@
+/**
+ * 技能数据
+ * 存储技能列表
+ */
+
+export const skills: string[] = [
+  'React',
+  'TypeScript',
+  'JavaScript',
+  'Vue.js',
+  'Node.js',
+  'Tailwind CSS',
+  'HTML/CSS',
+  'Git',
+  'Webpack',
+  'Vite',
+  'RESTful API',
+  'Responsive Design',
+]
+
